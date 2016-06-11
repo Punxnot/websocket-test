41a0cc3d5bca71a2119b982af9874c44b6b88d4b
Marina <do-you-speak@yandex.ru>
2016-06-12 01:52:46 +0300

    Connect socket.io

--- server/main.js
+++ server/main.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.get('/', function(req, res) {
-  res.send("Hello world.");
-  console.log("Something connected to express");
-})
+// app.get('/', function(req, res) {
+//   res.send("Hello world.");
+//   console.log("Something connected to express");
+// });
+
+app.use(express.static('app'));
+
+io.on('connection', function(socket) {
+  console.log("Something connected to Socket.io");
+  socket.emit("messages", ["Hello", "Hi there", "How are you?"]);
+});
 
 server.listen(80);
