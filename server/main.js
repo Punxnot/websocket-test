dc49b9fb27f6a2d3819885e5250c22d1d117ea36
Marina <do-you-speak@yandex.ru>
2016-06-13 23:56:53 +0300

    Add time, styles

--- server/main.js
+++ server/main.js
@@ -36,6 +36,7 @@ var messages = [{
 }];
 
 app.use(express.static('app'));
+app.use('/bower_components', express.static('bower_components'));
 
 io.on('connection', function(socket) {
   console.log("Something connected to Socket.io");
