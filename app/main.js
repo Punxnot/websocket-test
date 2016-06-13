a83d2c91672d3d13d5db8582c203bd3befb7ab43
Marina <do-you-speak@yandex.ru>
2016-06-13 22:59:14 +0300

    Add user session

--- app/main.js
+++ app/main.js
@@ -1,3 +1,11 @@
+var userId = localStorage.getItem("userId") || randomId();
+localStorage.setItem("userId", userId);
+console.info("Hi, I'm user #" + userId);
+
+function randomId() {
+  return Math.floor(Math.random() * 1e11);
+}
+
 var socket = io.connect('http://localhost', {'forceNew': true});
 socket.on("messages", function(data) {
   console.info(data);
@@ -9,6 +17,7 @@ socket.on("messages", function(data) {
       <a href=${data.content.link} class="message" target=blank>
         ${data.content.text}
       </a>
+      <input type=submit class="likes-count" value="${data.likedBy.length} Likes">
     `)
   }).join(" ");
 
@@ -22,6 +31,7 @@ function addMessage(e) {
       text: document.getElementById("message").value,
       link:  document.getElementById("linkAddress").value,
     },
+    likedBy: [],
     ts: Date.now()
   }
 
