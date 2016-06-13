d672cb6abf13fbab7873744b3b3fa7e8312dbe02
Marina <do-you-speak@yandex.ru>
2016-06-13 23:26:01 +0300

    Add likes

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 var userId = localStorage.getItem("userId") || randomId();
 localStorage.setItem("userId", userId);
 console.info("Hi, I'm user #" + userId);
+var messageCache;
 
 function randomId() {
   return Math.floor(Math.random() * 1e11);
@@ -9,27 +10,47 @@ function randomId() {
 var socket = io.connect('http://localhost', {'forceNew': true});
 socket.on("messages", function(data) {
   console.info(data);
-  var html = data.map(function(data) {
+  messageCache = data;
+  render();
+});
+
+function render() {
+  var data = messageCache;
+  var html = data.map(function(data, index) {
     return(`
-      <div class="name">
-        ${data.userName}
-      </div>
-      <a href=${data.content.link} class="message" target=blank>
-        ${data.content.text}
-      </a>
-      <input type=submit class="likes-count" value="${data.likedBy.length} Likes">
+      <form class="message" onsubmit="return likeMessage(messageCache[${index}])">
+        <div class="name">
+          ${data.userName}
+        </div>
+        <a href=${data.content.link} class="message" target=blank>
+          ${data.content.text}
+        </a>
+        <input type=submit class="likes-count" value="${data.likedBy.length} Likes">
+      </form>
     `)
   }).join(" ");
-
   document.getElementById("messages").innerHTML = html;
-});
+}
+
+function likeMessage(message) {
+  var index = message.likedBy.indexOf(userId);
+  if (index < 0) {
+    message.likedBy.push(userId);
+  } else {
+    message.likedBy.splice(index, 1);
+  }
+  socket.emit("update-message", message);
+  render();
+  return false;
+}
 
 function addMessage(e) {
   var payload = {
+    messageId: randomId(),
     userName: document.getElementById("username").value,
     content: {
       text: document.getElementById("message").value,
-      link:  document.getElementById("linkAddress").value,
+      link:  document.getElementById("linkAddress").value
     },
     likedBy: [],
     ts: Date.now()
